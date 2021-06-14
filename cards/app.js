81c93b8e727bc97054e651cb543ff5e313524127
Kseniia Prytkova <prytkovakseniia@gmail.com>
2021-06-14 13:16:27 +0300

    update active card logic

--- cards/app.js
+++ cards/app.js
@@ -1,18 +1,25 @@
-// get each slide in string format
-const slides = document.querySelectorAll('.slide');
-console.log('SLIDES: ', slides);
+function slidesPlugin(activeSlide = 1) {
+    // get each slide in string format
+    const slides = document.querySelectorAll('.slide');
+    // console.log('SLIDES: ', slides);
 
-// add a listener to each slide
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses();
+    // add class active to element - active element after reload
+    slides[activeSlide].classList.add('active');
 
-        slide.classList.add('active');
-    })
+    // add a listener to each slide
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+
+            slide.classList.add('active');
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        })
+    }
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    })
-}
+slidesPlugin(1);
